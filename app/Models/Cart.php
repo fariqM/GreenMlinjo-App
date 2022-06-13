4f71839247cd4c19fbca555b44ad9440dd08028d
naliphelvina <naliphelvina@gmail.com>
2022-06-13 21:08:34 +0700

    fix cart data to sensitive number

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -8,6 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Cart extends Model
 {
     use HasFactory;
+    
+    protected $casts = [
+        'qty' => 'integer',
+    ];
 
     protected $fillable = ['user_id', 'product_id', 'qty'];
 
